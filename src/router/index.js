798eea4ca2177e2b112f0a435938eba61e8e48fd
Clifton Hills <1966839714@qq.com>
2023-05-15 14:35:47 +0800

    VUE-5.15

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,14 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/other',
-    name: 'other',
     component: () => import('../views/OtherView.vue')
   }
 ]
